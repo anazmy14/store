0b0851c50b5e183c2cc1a100588b7eb3501926e0
anazmy14 <anazmy14@gmail.com>
2023-03-12 18:15:23 +0200

    update styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/header.module.css";
 import SideMenu from "./SideMenu";
 const Header = () => {
   return (
-    <header className={styles["header"]}>
-      <p className={styles["msg"]}>White Friday Sales Up To 70% Off</p>
+    <>
+      <header className={styles["header"]}>
+        <p className={styles["msg"]}>White Friday Sales Up To 70% Off</p>
+        <menu className={styles["menu"]}>
+          <SideMenu />
+
+          <Image
+            className={styles["logo"]}
+            src="/logo.svg"
+            width={24}
+            height={24}
+            alt="logo"
+          />
+          <span className={styles["brand"]}> Store Locator </span>
+          <div className={styles["search"]}>
+            <input type="search" placeholder="Search" />
+            <button className={styles["search-btn"]}>
+              <Image src="/search.svg" width={24} height={24} alt="search" />
+            </button>
+          </div>
+
+          <div className={styles["btns-box"]}>
+            <button className={styles["lang-btn"]}> العربية </button>
+            <button>
+              <Image src="/user.svg" width={24} height={24} alt="profile" />
+            </button>
+            <button>
+              <Image src="/cart.svg" width={24} height={24} alt="cart" />
+            </button>
+          </div>
+        </menu>
+      </header>
       <nav className={styles["nav"]}>
-        <SideMenu />
-
-        <Image
-          className={styles["logo"]}
-          src="/logo.svg"
-          width={24}
-          height={24}
-          alt="logo"
-        />
-        <span className={styles["brand"]}> Store Locator </span>
-        <div className={styles["search"]}>
-          <input type="search" placeholder="Search" />
-          <button className={styles["search-btn"]}>
-            <Image src="/search.svg" width={24} height={24} alt="search" />
-          </button>
-        </div>
-
-        <div className={styles["btns-box"]}>
-          <button className={styles["lang-btn"]}> العربية </button>
-          <button>
-            <Image src="/user.svg" width={24} height={24} alt="profile" />
-          </button>
-          <button>
-            <Image src="/cart.svg" width={24} height={24} alt="cart" />
-          </button>
-        </div>
+        <Link href="/"> Top Deals </Link>
+        <Link href="/"> Deals of the Day </Link>
+        <Link href="/"> Men </Link>
+        <Link href="/"> Women </Link>
+        <Link href="/"> New </Link>
+        <Link href="/"> Brands </Link>
+        <Link href="/"> Sports </Link>
+        <Link href="/"> Accessories </Link>
+        <Link href="/"> Sale </Link>
       </nav>
-    </header>
+    </>
   );
 };
 
